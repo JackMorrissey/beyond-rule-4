4fad95d03675b0b5faadea550f23c32b790a38b5
Jack Morrissey <code@jmmorrissey.com>
2018-04-08 15:57:13 -0500

    refactor(forecast.model): extract the computation to the class
    Move out of milestones

--- src/app/milestones/milestones.component.ts
+++ src/app/milestones/milestones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { round } from '../utilities/number-utility';
 
@@ -13,17 +13,17 @@ import { Milestones } from './milestone.model';
 })
 export class MilestonesComponent implements OnInit {
 
-  calculateInput: CalculateInput;
+  @Input() calculateInput: CalculateInput;
   milestones: Milestones;
   forecast: Forecast;
 
   constructor() {
     const input = new CalculateInput();
-    input.annualExpenses = 28000;
+    input.annualExpenses = 30000;
     input.annualSafeWithdrawalRate = 0.04;
     input.expectedAnnualGrowthRate = 0.08;
     input.leanFiPercentage = 0.7;
-    input.netWorth = 155000;
+    input.netWorth = 300000;
     input.monthlyContribution = 3000;
     this.calculateInput = input;
   }
@@ -35,50 +35,6 @@ export class MilestonesComponent implements OnInit {
   calculate() {
     const fiNumber = 1 / this.calculateInput.annualSafeWithdrawalRate * this.calculateInput.annualExpenses;
     this.milestones = new Milestones(fiNumber, this.calculateInput.leanFiPercentage);
-    const stopForecastingAmount = fiNumber * 1.3; // default to ludicrous Fi.
-    this.forecast = this.getForecast(stopForecastingAmount);
+    this.forecast = new Forecast(this.calculateInput);
   }
-
-  private getForecast(stopForecastingAmount: number): Forecast {
-    const annualExpenses = this.calculateInput.annualExpenses;
-    const monthlyAverageGrowth = 1 + this.calculateInput.expectedAnnualGrowthRate / 12;
-    const startingNetWorth = this.calculateInput.netWorth;
-    let currentNetWorth = startingNetWorth;
-    let totalContributions = currentNetWorth; // can't yet delve into the past
-    let month = 0;
-    const monthlyForecasts = [new MonthlyForecast({
-      monthIndex: 0,
-      netWorth: startingNetWorth,
-      lastMonthNetWorth: 0,
-      contribution: 0,
-      interestGains: 0,
-      timesAnnualExpenses: round(startingNetWorth / annualExpenses),
-      totalContributions: totalContributions,
-      totalInterestGains: 0
-    })];
-    while (currentNetWorth < stopForecastingAmount && month < 1000) {
-      const contribution = this.calculateInput.monthlyContribution;
-      const newNetWorth = round(((currentNetWorth + contribution) * 100 * monthlyAverageGrowth) / 100);
-      const interestGain = round(newNetWorth - currentNetWorth - contribution);
-      const timesAnnualExpenses = round(newNetWorth / annualExpenses);
-      month++;
-      totalContributions += contribution;
-      const totalInterestGains = round(newNetWorth - totalContributions);
-      monthlyForecasts.push(new MonthlyForecast({
-        monthIndex: month,
-        netWorth: newNetWorth,
-        lastMonthNetWorth: currentNetWorth,
-        contribution: contribution,
-        interestGains: interestGain,
-        timesAnnualExpenses: timesAnnualExpenses,
-        totalContributions: totalContributions,
-        totalInterestGains: totalInterestGains
-      }));
-      currentNetWorth = newNetWorth;
-    }
-    return new Forecast({
-      monthlyForecasts: monthlyForecasts
-    });
-  }
-
 }
--- src/app/models/forecast.model.ts
+++ src/app/models/forecast.model.ts
@@ -1,23 +1,71 @@
+import { CalculateInput } from './calculate-input.model';
+import { round } from '../utilities/number-utility';
+
 export class Forecast {
   monthlyForecasts: MonthlyForecast[];
-  monthIndex0Date: Date;
 
-  public constructor(init?: Partial<Forecast>) {
-    Object.assign(this, init);
-    if (!this.monthIndex0Date) {
-      this.monthIndex0Date = new Date();
-      this.monthIndex0Date.setDate(1);
+  public constructor(calculateInput: CalculateInput, public month0Date: Date = new Date()) {
+
+    this.computeForecast(calculateInput);
+    this.setDates();
+  }
+
+  private computeForecast(calculateInput: CalculateInput) {
+    const fiNumber = 1 / calculateInput.annualSafeWithdrawalRate * calculateInput.annualExpenses;
+    const stopForecastingAmount = fiNumber * 1.3; // default to a bit more than Fat Fi.
+
+    const annualExpenses = calculateInput.annualExpenses;
+    const monthlyAverageGrowth = 1 + calculateInput.expectedAnnualGrowthRate / 12;
+    const startingNetWorth = calculateInput.netWorth;
+    let currentNetWorth = startingNetWorth;
+    let totalContributions = currentNetWorth; // can't yet delve into the past
+    let month = 0;
+    const monthlyForecasts = [new MonthlyForecast({
+      monthIndex: 0,
+      netWorth: startingNetWorth,
+      lastMonthNetWorth: 0,
+      contribution: 0,
+      interestGains: 0,
+      timesAnnualExpenses: round(startingNetWorth / annualExpenses),
+      totalContributions: totalContributions,
+      totalInterestGains: 0
+    })];
+    while (currentNetWorth < stopForecastingAmount && month < 1000) {
+      const contribution = calculateInput.monthlyContribution;
+      const newNetWorth = round(((currentNetWorth + contribution) * 100 * monthlyAverageGrowth) / 100);
+      const interestGain = round(newNetWorth - currentNetWorth - contribution);
+      const timesAnnualExpenses = round(newNetWorth / annualExpenses);
+      month++;
+      totalContributions += contribution;
+      const totalInterestGains = round(newNetWorth - totalContributions);
+      monthlyForecasts.push(new MonthlyForecast({
+        monthIndex: month,
+        netWorth: newNetWorth,
+        lastMonthNetWorth: currentNetWorth,
+        contribution: contribution,
+        interestGains: interestGain,
+        timesAnnualExpenses: timesAnnualExpenses,
+        totalContributions: totalContributions,
+        totalInterestGains: totalInterestGains
+      }));
+      currentNetWorth = newNetWorth;
     }
-    const firstMonth = this.monthIndex0Date.getMonth();
+    this.monthlyForecasts = monthlyForecasts;
+  }
+
+  private setDates() {
+    if (!this.month0Date) {
+      this.month0Date = new Date();
+      this.month0Date.setDate(1);
+    }
+    const firstMonth = this.month0Date.getMonth();
     this.monthlyForecasts.forEach(monthlyForecast => {
-      const forecastDate = new Date(this.monthIndex0Date);
+      const forecastDate = new Date(this.month0Date);
       forecastDate.setMonth(firstMonth + monthlyForecast.monthIndex);
-      const month = forecastDate.getMonth() + 1;
-      const monthString = month >= 10 ? `${month}` : `0${month}`;
-      // monthlyForecast.dateDisplay = `${forecastDate.getFullYear()}-${monthString}`;
       monthlyForecast.date = forecastDate;
     });
   }
+
 }
 
 export class MonthlyForecast {
