7686c13ef77e20a1e13265027a9cf188b39a5bcd
Jack Morrissey <code@jmmorrissey.com>
2018-05-20 21:07:35 -0500

    feat(output): forecast above milestones

--- src/app/forecasting/input/ynab/ynab.component.ts
+++ src/app/forecasting/input/ynab/ynab.component.ts
@@ -60,12 +60,11 @@ export class YnabComponent implements OnInit {
     this.categoryGroupsWithCategories = await this.ynabService.getCategoryGroupsWithCategories(budgetId);
 
     this.netWorth = this.getNetWorth(this.accounts);
-    this.categoriesDisplay = this.getCategoriesDisplay(this.categoryGroupsWithCategories, this.currentMonth);
-    this.resetForm(this.categoriesDisplay);
+    const categoryGroups = this.mapCategoryGroups(this.categoryGroupsWithCategories, this.currentMonth);
+    this.resetForm(categoryGroups);
 
     const formChanges = this.budgetForm.valueChanges.pipe(debounce(() => timer(500)));
     formChanges.subscribe(() => {
-      console.log(JSON.stringify(this.budgetForm.value.categoryGroups));
       this.updateInput();
     });
 
@@ -73,7 +72,6 @@ export class YnabComponent implements OnInit {
   }
 
   updateInput() {
-    // this.monthlyExpenses = this.getMonthlyExpenses(this.categoriesDisplay);
     this.monthlyExpenses = this.getMonthlyExpenses(this.budgetForm.value.categoryGroups);
     this.annualExpenses = this.monthlyExpenses * 12;
     const result = new CalculateInput();
@@ -84,10 +82,10 @@ export class YnabComponent implements OnInit {
     this.calculateInputChange.emit(result);
   }
 
-  private getMonthlyExpenses(categoriesDisplay) {
-    const expenses = categoriesDisplay.map(categoryGroup => {
+  private getMonthlyExpenses(categoryGroups) {
+    const expenses = categoryGroups.map(categoryGroup => {
       return categoryGroup.categories.map(category => {
-        return category.budgeted;
+        return category.fiBudget;
       }).reduce((prev, next) => {
         return prev + next;
       });
@@ -111,7 +109,7 @@ export class YnabComponent implements OnInit {
     return ynab.utils.convertMilliUnitsToCurrencyAmount(netWorth);
   }
 
-  private getCategoriesDisplay(categoryGroupsWithCategories: ynab.CategoryGroupWithCategories[], monthDetail: ynab.MonthDetail) {
+  private mapCategoryGroups(categoryGroupsWithCategories: ynab.CategoryGroupWithCategories[], monthDetail: ynab.MonthDetail) {
     if (!categoryGroupsWithCategories) {
       return [];
     }
@@ -136,6 +134,24 @@ export class YnabComponent implements OnInit {
     return categoryGroups;
   }
 
+  private mapCategory(category: ynab.Category, childrenIgnore: boolean, monthDetail: ynab.MonthDetail) {
+    const ignore = childrenIgnore || category.hidden;
+    const found = monthDetail.categories.find(c => category.id === c.id);
+    const retrievedBudgeted = !found ? 0 : ynab.utils.convertMilliUnitsToCurrencyAmount(found.budgeted);
+    const computedFiBudget = ignore ? 0 : retrievedBudgeted;
+    const computedLeanFiBudget = computedFiBudget * .7;
+
+    return {
+      name: category.name,
+      ignore,
+      hidden: category.hidden,
+      id: category.id,
+      retrievedBudgeted,
+      computedFiBudget,
+      computedLeanFiBudget,
+    };
+  }
+
   private resetForm(categoriesDisplay) {
     const categoryGroupFormGroups = categoriesDisplay.map(cg => this.formBuilder.group({
       name: cg.name,
@@ -143,29 +159,16 @@ export class YnabComponent implements OnInit {
       hidden: cg.id,
       categories: this.formBuilder.array(cg.categories.map(c => this.formBuilder.group({
         name: c.name,
-        budgeted: c.budgeted,
         retrievedBudgeted: c.retrievedBudgeted,
+        computedFiBudget: c.computedFiBudget,
+        computedLeanFiBudget: c.computedLeanFiBudget,
+        fiBudget: c.computedFiBudget,
+        leanFiBudget: c.computedLeanFiBudget,
         ignore: c.ignore
       })))
     }));
     const categoryGroups = this.formBuilder.array(categoryGroupFormGroups);
     this.budgetForm.setControl('categoryGroups', categoryGroups);
-  }
-
-  private mapCategory(category: ynab.Category, childrenIgnore: boolean, monthDetail: ynab.MonthDetail) {
-    const ignore = childrenIgnore || category.hidden;
-    const found = monthDetail.categories.find(c => category.id === c.id);
-    const retrievedBudgeted = !found ? 0 : ynab.utils.convertMilliUnitsToCurrencyAmount(found.budgeted);
-    const budgeted = ignore ? 0 : retrievedBudgeted;
-
-    return {
-      name: category.name,
-      ignore,
-      hidden: category.hidden,
-      id: category.id,
-      budgeted,
-      retrievedBudgeted,
-    };
   }
 
 }
