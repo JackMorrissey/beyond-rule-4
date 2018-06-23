4e2e18b96bb4e4858d164260c6b2057638b01184
Jack Morrissey <code@jmmorrissey.com>
2018-06-23 13:16:32 -0500

    feat(ynab): disable feature for release
    Thought the fancy new theme and whatnot were worth a release. Side-project
    feature toggle yo

--- src/app/forecasting/input/ynab/ynab.service.ts
+++ src/app/forecasting/input/ynab/ynab.service.ts
@@ -5,8 +5,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import * as ynab from 'ynab';
 
-import { devAccessToken } from './token.secret';
-import { SampleData } from './sample-data.secret';
+// import { devAccessToken } from './token.secret';
+// import { SampleData } from './sample-data.secret';
 
 
 @Injectable()
@@ -16,49 +16,38 @@ export class YnabService {
   private useSampleData = false;
 
   constructor(private http: Http) {
-    this.ynabApi = new ynab.api(devAccessToken);
   }
 
   async getBudgets(): Promise<ynab.BudgetSummary[]> {
-    if (this.useSampleData) {
-      return SampleData.Budgets;
-    }
+
 
     const budgets = await this.ynabApi.budgets.getBudgets();
     return budgets.data.budgets;
   }
 
   async getMonths(budgetId: string): Promise<ynab.MonthSummary[]> {
-    if (this.useSampleData) {
-      return SampleData.Months;
-    }
+
 
     const months = await this.ynabApi.months.getBudgetMonths(budgetId);
     return months.data.months;
   }
 
   async getMonth(budgetId: string, budgetMonth: Date | 'current'): Promise<ynab.MonthDetail> {
-    if (this.useSampleData) {
-      return SampleData.Month;
-    }
+
 
     const month = await this.ynabApi.months.getBudgetMonth(budgetId, budgetMonth);
     return month.data.month;
   }
 
   async getAccounts(budgetId: string): Promise<ynab.Account[]> {
-    if (this.useSampleData) {
-      return SampleData.Accounts;
-    }
+
 
     const accounts = await this.ynabApi.accounts.getAccounts(budgetId);
     return accounts.data.accounts;
   }
 
   async getCategoryGroupsWithCategories(budgetId: string): Promise<ynab.CategoryGroupWithCategories[]> {
-    if (this.useSampleData) {
-      return SampleData.CategoryGroupsWithCategories;
-    }
+
 
     const categories = await this.ynabApi.categories.getCategories(budgetId);
     return categories.data.category_groups;
