54b6b4e838828ddf170aeee8c774bbcc09222351
Brian D. Adams <briandadams@gmail.com>
2020-05-17 15:11:45 -0500

    Define missing declarations and imports

--- src/app/forecasting/output/milestones/chart/chart.component.spec.ts
+++ src/app/forecasting/output/milestones/chart/chart.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChartComponent } from './chart.component';
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -11,7 +12,7 @@ describe('ChartComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ChartComponent],
-      imports: [NgxChartsModule]
+      imports: [NgxChartsModule, BrowserAnimationsModule]
     })
       .compileComponents();
   }));
--- src/app/forecasting/output/milestones/milestones.component.spec.ts
+++ src/app/forecasting/output/milestones/milestones.component.spec.ts
@@ -2,15 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MilestonesComponent } from './milestones.component';
 
+import { ChartComponent } from './chart/chart.component';
+import { TextComponent } from './text/text.component';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('MilestonesComponent', () => {
   let component: MilestonesComponent;
   let fixture: ComponentFixture<MilestonesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MilestonesComponent ]
+      declarations: [MilestonesComponent, ChartComponent, TextComponent],
+      imports: [NgxChartsModule, BrowserAnimationsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
