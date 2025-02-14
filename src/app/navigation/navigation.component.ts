49fe4ec11bd9cd0df8b6000e16c7e48c15487e4d
swissbuechi <raphael.buechi@hotmail.com>
2025-02-14 18:33:17 +0100

    Fixed button and PWA colors in dark-mode

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -9,12 +9,17 @@ import { YnabApiService } from '../ynab-api/ynab-api.service';
 })
 
 export class NavigationComponent implements OnInit {
+  navbarClass: string = 'bg-light'; // Default to light theme
+  buttonClass: string = 'btn-outline-dark'; // Default to dark color
   isOpen = false;
   isAuthorized = false;
 
   constructor(private ynabApiService: YnabApiService) { }
 
   ngOnInit() {
+    const theme = document.documentElement.getAttribute('data-bs-theme');
+    this.navbarClass = theme === 'dark' ? 'bg-dark' : 'bg-light';
+    this.buttonClass = theme === 'dark' ? 'btn-outline-light' : 'btn-outline-dark';
     this.ynabApiService.isAuthorized$.subscribe({next: (isAuthorized) => this.isAuthorized = isAuthorized});
    }
 
