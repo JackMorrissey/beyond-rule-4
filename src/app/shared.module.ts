1534723e6f31b01f51bb0cd0c20bbea346254408
swissbuechi <raphael.buechi@hotmail.com>
2025-02-14 14:09:15 +0100

    Upgrade to Angular 18

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,34 +1,56 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
 import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
-import { faRocket, faForward, faRunning, faExternalLinkSquareAlt, faInfoCircle, faCheckCircle, faSync, faPlusCircle, faKey, faCaretSquareDown} from '@fortawesome/free-solid-svg-icons';
+import {
+  faRocket,
+  faForward,
+  faRunning,
+  faExternalLinkSquareAlt,
+  faInfoCircle,
+  faCheckCircle,
+  faSync,
+  faPlusCircle,
+  faKey,
+  faCaretSquareDown,
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
+  exports: [CommonModule, NgbModule, RouterModule, FontAwesomeModule],
+  declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     NgbModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
   ],
-  exports: [
-    CommonModule,
-    HttpClientModule,
-    NgbModule,
-    RouterModule,
-    FontAwesomeModule
-  ],
-  declarations: [],
-  providers: [],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class SharedModule {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faCheckSquare, faRocket, faForward, faRunning, faExternalLinkSquareAlt, faInfoCircle, faCheckCircle, faSync, faPlusCircle, faKey, faCaretSquareDown);
+    library.addIcons(
+      faCheckSquare,
+      faRocket,
+      faForward,
+      faRunning,
+      faExternalLinkSquareAlt,
+      faInfoCircle,
+      faCheckCircle,
+      faSync,
+      faPlusCircle,
+      faKey,
+      faCaretSquareDown
+    );
   }
 }
